748506a07f0ac2d2851fbff27279b28a1f68d5fe
Provkin.VA <prowkan@gmail.com>
2025-09-03 17:49:00 +0700

    Исправлена кодировка консоли.

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include <Windows.h>
+
 int main()
 {
+	UINT OldConsoleCP = GetConsoleCP();
+	UINT OldConsoleOutputCP = GetConsoleOutputCP();
+
+	(void)SetConsoleCP(65001);
+	(void)SetConsoleOutputCP(65001);
+
 	std::cout << "Тест, тест, тест." << std::endl;
+
+	(void)SetConsoleCP(OldConsoleCP);
+	(void)SetConsoleOutputCP(OldConsoleOutputCP);
 
 	return 0;
 }
